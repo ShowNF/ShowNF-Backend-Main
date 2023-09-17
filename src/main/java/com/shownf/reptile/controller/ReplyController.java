c84dc04201d18c3ae3fe4bdd12a4060eb0e3538d
Lee <lsmin476@naver.com>
2023-09-18 01:53:41 +0900

    |SM UPDATE| 대댓글 컨트롤러 Swagger UI 수정

--- src/main/java/com/shownf/reptile/controller/ReplyController.java
+++ src/main/java/com/shownf/reptile/controller/ReplyController.java
@@ -4,6 +4,7 @@ import com.shownf.reptile.Model.DTO.RequestReplyDeleteDTO;
 import com.shownf.reptile.Model.DTO.RequestReplySaveDTO;
 import com.shownf.reptile.Model.DTO.RequestReplysDTO;
 import com.shownf.reptile.service.ReplyService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +25,17 @@ public class ReplyController {
         this.replyService = replyService;
     }
 
+
     // 대댓글 전체 조회
+    @ApiOperation(value = "대댓글 전체 조회", notes = "댓글아이디로 찾은 대댓글 전체 조회")
     @GetMapping("reply/comment/{commentId}")
     public List<RequestReplysDTO> getReplys(@PathVariable Long commentId){
         return replyService.getReplys(commentId);
     }
 
+
     // 대댓글 저장
+    @ApiOperation(value = "대댓글 저장", notes = "댓글에 댓글 작성시 저장")
     @PostMapping("reply")
     public ResponseEntity<Map<String, Object>> saveReply(@RequestBody RequestReplySaveDTO requestReplySaveDTO){
         Long replyId = replyService.saveReply(requestReplySaveDTO);
@@ -46,7 +51,9 @@ public class ReplyController {
         return ResponseEntity.status(httpStatus).body(requestMap);
     }
 
+
     // 대댓글 삭제
+    @ApiOperation(value = "대댓글 삭제", notes = "댓글에 댓글 삭제시 삭제")
     @DeleteMapping("reply")
     public ResponseEntity<Map<String, Object>> deleteComment(@RequestBody RequestReplyDeleteDTO requestReplyDeleteDTO){
         Long replyId = replyService.deleteReply(requestReplyDeleteDTO);
