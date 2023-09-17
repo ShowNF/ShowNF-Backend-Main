68cd5cdcacab8eb2c760a608bb0d1c71b9a15416
Lee <lsmin476@naver.com>
2023-09-18 01:54:08 +0900

    |SM UPDATE| 분양글 컨트롤러 Swagger UI 수정

--- src/main/java/com/shownf/reptile/controller/SaleController.java
+++ src/main/java/com/shownf/reptile/controller/SaleController.java
@@ -3,6 +3,7 @@ package com.shownf.reptile.controller;
 import com.shownf.reptile.Model.DTO.RequestSaleSaveDTO;
 import com.shownf.reptile.Model.DTO.ResponseSaleDTO;
 import com.shownf.reptile.service.SaleService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,25 +27,33 @@ public class SaleController {
         this.saleService = saleService;
     }
 
+
     // 분양글 조회
+    @ApiOperation(value = "분양글 조회", notes = "분양글 아이디로 분양글 조회")
     @GetMapping("sale/{saleId}")
     public ResponseSaleDTO getSale(@PathVariable Long saleId){
         return saleService.getSale(saleId);
     }
 
+
     // 분양글 Page 로 전체 조회
+    @ApiOperation(value = "분양글 전체 조회", notes = "분양글 5개씩 페이징 조회")
     @GetMapping("sale/all")
     public Page<ResponseSaleDTO> getSales(@PageableDefault(size=12)Pageable pageable){
         return saleService.getSales(pageable);
     }
 
+
     // 유저가 좋아요한 분양글 조회
+    @ApiOperation(value = "내가 좋아요한 분양글 조회", notes = "유저 아이디로 좋아요 누른 분양글 조회")
     @GetMapping("sale/user/{userId}")
     public List<ResponseSaleDTO> getUserSaleHearts(@PathVariable String userId){
         return saleService.getUserSaleHearts(userId);
     }
 
+
     // 분양글 저장
+    @ApiOperation(value = "분양글 저장", notes = "분양글 작성시 저장")
     @PostMapping("sale")
     public ResponseEntity<Map<String, Object>> saveSale(@RequestBody RequestSaleSaveDTO requestSaleSaveDTO){
         Long saleId = saleService.saveSale(requestSaleSaveDTO);
