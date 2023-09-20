8c53feddc53da1e0328fb53dd6bfb7f7f355e81b
Lee <lsmin476@naver.com>
2023-09-21 02:28:41 +0900

    |SM UPDATE| small Bean ChatRoomDAO 객체 생성 및 레포지토리 저장

---
+++ src/main/java/com/shownf/reptile/bean/small/SaveChatRoomDAOBean.java
@@ -0,0 +1,42 @@
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.RequestChatRoomSaveDTO;
+import com.shownf.reptile.Model.entity.ChatRoomDAO;
+import com.shownf.reptile.repository.ChatRoomRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class SaveChatRoomDAOBean {
+
+    ChatRoomRepositoryJPA chatRoomRepositoryJPA;
+
+    @Autowired
+    public SaveChatRoomDAOBean(ChatRoomRepositoryJPA chatRoomRepositoryJPA) {
+        this.chatRoomRepositoryJPA = chatRoomRepositoryJPA;
+    }
+
+    // 채팅방 저장하기
+    public void exec(ChatRoomDAO chatRoomDAO){
+        chatRoomRepositoryJPA.save(chatRoomDAO);
+    }
+
+    public void exec(Long uniqueId, RequestChatRoomSaveDTO requestChatRoomSaveDTO){
+
+        // 채팅방 아이디
+        String chatRoomId = requestChatRoomSaveDTO.getChatRoomId();
+
+        // 파는사람 아이디
+        String sellerId = requestChatRoomSaveDTO.getSellerId();
+
+        // 사는사람 아이디
+        String buyerId = requestChatRoomSaveDTO.getBuyerId();
+
+        // 시간
+        LocalDateTime uploadTime = LocalDateTime.now();
+
+        exec(new ChatRoomDAO(uniqueId, chatRoomId, sellerId, buyerId, uploadTime));
+    }
+}
