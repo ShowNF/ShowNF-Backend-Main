6bbaf326b97b3c2535a9c83ecb3f451042e38f64
Lee <lsmin476@naver.com>
2023-09-21 13:11:03 +0900

    |SM UPDATE| small Bean 판매 채팅방 DAOs DTOs 변환

--- src/main/java/com/shownf/reptile/bean/small/CreateChatRoomsDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateChatRoomsDTOBean.java
@@ -1,2 +1,31 @@
-package com.shownf.reptile.bean.small;public class CreateChatRoomsDTOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.ResponseChatRoomDTO;
+import com.shownf.reptile.Model.entity.ChatRoomDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CreateChatRoomsDTOBean {
+
+    public List<ResponseChatRoomDTO> exec(List<ChatRoomDAO> chatRoomDAOs){
+
+        List<ResponseChatRoomDTO> responseChatRoomDTOs = new ArrayList<>();
+
+        // DTO 객체에 게시물 정보 넘기기
+        for (ChatRoomDAO chatRoomDAO : chatRoomDAOs){
+            ResponseChatRoomDTO responseChatRoomDTO = new ResponseChatRoomDTO();
+
+            responseChatRoomDTO.setUniqueId(responseChatRoomDTO.getUniqueId());
+            responseChatRoomDTO.setChatRoomId(chatRoomDAO.getChatRoomId());
+            responseChatRoomDTO.setSellerId(chatRoomDAO.getSellerId());
+            responseChatRoomDTO.setBuyerId(chatRoomDAO.getBuyerId());
+            responseChatRoomDTO.setUploadTime(chatRoomDAO.getUploadTime());
+
+            responseChatRoomDTOs.add(responseChatRoomDTO);
+        }
+        return responseChatRoomDTOs;
+    }
 }
