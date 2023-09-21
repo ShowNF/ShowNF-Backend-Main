5b2fdc9ef5f4cc176a5f0fe3c60e844e24e93593
Lee <lsmin476@naver.com>
2023-09-21 13:09:12 +0900

    |SM ADD| 판매 채팅방 조회하기 위한 클래스 생성

---
+++ src/main/java/com/shownf/reptile/bean/GetSellerChatRoomsBean.java
@@ -0,0 +1,13 @@
+package com.shownf.reptile.bean;
+
+import com.shownf.reptile.Model.DTO.ResponseChatRoomDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetChatRoomsBean {
+
+    // 채팅방 전체 조회
+    public List<ResponseChatRoomDTO> exec()
+}
