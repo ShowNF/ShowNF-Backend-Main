db47ad822b418293f1e61ebde50448a0deca5c2a
Lee <lsmin476@naver.com>
2023-09-22 00:39:50 +0900

    |SM UPDATE| small Bean 구매 채팅방 DAOs 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetBuyerChatRoomsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetBuyerChatRoomsDAOBean.java
@@ -1,2 +1,25 @@
-package com.shownf.reptile.bean.small;public class GetBuyerChatRoomsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.ChatRoomDAO;
+import com.shownf.reptile.repository.ChatRoomRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetBuyerChatRoomsDAOBean {
+
+    ChatRoomRepositoryJPA chatRoomRepositoryJPA;
+
+    @Autowired
+    public GetBuyerChatRoomsDAOBean(ChatRoomRepositoryJPA chatRoomRepositoryJPA) {
+        this.chatRoomRepositoryJPA = chatRoomRepositoryJPA;
+    }
+
+    // 구매하는 경우의 채팅방 조회
+    public List<ChatRoomDAO> exec(String userId){
+        return chatRoomRepositoryJPA.findByBuyerId(userId);
+    }
+
 }
