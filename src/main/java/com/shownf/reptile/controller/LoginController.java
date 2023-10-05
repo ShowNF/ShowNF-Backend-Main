6f8e0bb467d5ed203619fcc1d4aa867af39f8221
Lee <lsmin476@naver.com>
2023-10-06 01:57:10 +0900

    |SM UPDATE| Service, Controller 구글 로그인 test

--- src/main/java/com/shownf/reptile/controller/LoginController.java
+++ src/main/java/com/shownf/reptile/controller/LoginController.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.controller;public class LoginController {
+package com.shownf.reptile.controller;
+
+import com.shownf.reptile.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+public class LoginController {
+
+    LoginService loginService;
+
+    @Autowired
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @GetMapping("/login/oauth2/code/{registrationId}")
+    public void googleLogin(@RequestParam String code, @PathVariable String registrationId) {
+        loginService.socialLogin(code, registrationId);
+    }
 }
