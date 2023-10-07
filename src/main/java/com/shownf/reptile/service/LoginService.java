5610c9cf2cfcbff7b51c8ae00eca3b05c53516a2
Lee <lsmin476@naver.com>
2023-10-07 10:06:15 +0900

    |SM UPDATE| properties 접근을 위한 Environment -> @Value 변경

--- src/main/java/com/shownf/reptile/service/LoginService.java
+++ src/main/java/com/shownf/reptile/service/LoginService.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shownf.reptile.Model.KakaoProfile;
 import com.shownf.reptile.Model.OAuthToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,33 @@ import org.springframework.web.client.RestTemplate;
 public class LoginService {
     private final Environment env;
     private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${GOOGLE_CLIENT_ID}")
+    String google_clientId;
+
+    @Value("${GOOGLE_CLIENT_SECRET}")
+    String google_clientSecret;
+
+    @Value("${GOOGLE_REDIRECT_URI}")
+    String google_redirectUri;
+
+    @Value("${GOOGLE_TOKEN_URI}")
+    String google_tokenUri;
+
+    @Value("${GOOGLE_RESOURCE_URI}")
+    String google_resourceUri;
+
+    @Value("${KAKAO_CLIENT_ID}")
+    String kakao_client_id;
+
+    @Value("${KAKAO_REDIRECT_URI}")
+    String kakao_redirect_uri;
+
+    @Value("${KAKAO_TOKEN_URL}")
+    String kakao_token_url;
+
+    @Value("${KAKAO_PROFILE_URL}")
+    String kakao_profile_url;
 
     @Autowired
     public LoginService(Environment env) {
@@ -62,16 +90,16 @@ public class LoginService {
 
 
     private String getGoogleAccessToken(String authorizationCode, String registrationId) {
-        String clientId = env.getProperty("oauth2." + registrationId + ".client-id");
+        /*String clientId = env.getProperty("oauth2." + registrationId + ".client-id");
         String clientSecret = env.getProperty("oauth2." + registrationId + ".client-secret");
         String redirectUri = env.getProperty("oauth2." + registrationId + ".redirect-uri");
-        String tokenUri = env.getProperty("oauth2." + registrationId + ".token-uri");
+        String tokenUri = env.getProperty("oauth2." + registrationId + ".token-uri");*/
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("code", authorizationCode);
-        params.add("client_id", clientId);
-        params.add("client_secret", clientSecret);
-        params.add("redirect_uri", redirectUri);
+        params.add("client_id", google_clientId);
+        params.add("client_secret", google_clientSecret);
+        params.add("redirect_uri", google_redirectUri);
         params.add("grant_type", "authorization_code");
 
         HttpHeaders headers = new HttpHeaders();
@@ -79,31 +107,31 @@ public class LoginService {
 
         HttpEntity entity = new HttpEntity(params, headers);
 
-        ResponseEntity<JsonNode> responseNode = restTemplate.exchange(tokenUri, HttpMethod.POST, entity, JsonNode.class);
+        ResponseEntity<JsonNode> responseNode = restTemplate.exchange(google_tokenUri, HttpMethod.POST, entity, JsonNode.class);
         JsonNode accessTokenNode = responseNode.getBody();
         return accessTokenNode.get("access_token").asText();
     }
 
 
     private JsonNode getGoogleUserResource(String accessToken, String registrationId) {
-        String resourceUri = env.getProperty("oauth2."+registrationId+".resource-uri");
+        /*String resourceUri = env.getProperty("oauth2."+registrationId+".resource-uri");*/
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + accessToken);
         HttpEntity entity = new HttpEntity(headers);
-        return restTemplate.exchange(resourceUri, HttpMethod.GET, entity, JsonNode.class).getBody();
+        return restTemplate.exchange(google_resourceUri, HttpMethod.GET, entity, JsonNode.class).getBody();
     }
 
 
     public OAuthToken getKakaoAccessToken(String code) {
-        String client_id = env.getProperty("kakao.client-id");
+        /*String client_id = env.getProperty("kakao.client-id");
         String redirect_uri = env.getProperty("kakao.redirect-uri");
-        String token_url = env.getProperty("kakao.token-url");
+        String token_url = env.getProperty("kakao.token-url");*/
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
-        params.add("client_id", client_id);
-        params.add("redirect_uri", redirect_uri);
+        params.add("client_id", kakao_client_id);
+        params.add("redirect_uri", kakao_redirect_uri);
         params.add("code", code);
 
         HttpHeaders headers = new HttpHeaders();
@@ -112,7 +140,7 @@ public class LoginService {
         HttpEntity entity = new HttpEntity(params, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(token_url, HttpMethod.POST, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(kakao_token_url, HttpMethod.POST, entity, String.class);
 
         ObjectMapper objectMapper = new ObjectMapper();
         OAuthToken oAuthToken = null;
@@ -132,13 +160,13 @@ public class LoginService {
         headers.add("Authorization", "Bearer " + accessToken);
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        String profile_url = env.getProperty("kakao.profile-url");
+        /*String profile_url = env.getProperty("kakao.profile-url");*/
 
         HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers);
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.exchange(
-                profile_url,
+                kakao_profile_url,
                 HttpMethod.POST,
                 kakaoProfileRequest,
                 String.class
