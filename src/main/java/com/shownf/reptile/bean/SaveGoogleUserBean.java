af850613f4cfa80f114cd642321e9cea943120de
Lee <lsmin476@naver.com>
2023-10-11 16:27:07 +0900

    |SM UPDATE| 구글 유저 저장시 필드 잘 못들어가는 에러 수정

--- src/main/java/com/shownf/reptile/bean/SaveGoogleUserBean.java
+++ src/main/java/com/shownf/reptile/bean/SaveGoogleUserBean.java
@@ -26,23 +26,32 @@ public class SaveGoogleUserBean {
     }
 
     public void exec(String accessToken, JsonNode userResourceNode){
+
+        // 구글 고유 아이디
         String id = userResourceNode.get("id").asText();
+
+        // 이름
         String name = userResourceNode.get("name").asText();
+
+        // 프로필 사진
         String picture = userResourceNode.get("picture").asText();
 
+        // 토큰 만료시간
         LocalDateTime localDateTime = LocalDateTime.now();
         localDateTime.plusHours(12);
 
+        // 아이디로 구글 유저 객체 찾기
         GoogleUserDAO googleUserDAO = googleUserRepositoryJPA.findByGoogleId(id);
 
+        // 아이디가 이미 존재하는지에 따라 로그인 및 회원가입
         if(googleUserDAO == null){
-            googleUserRepositoryJPA.save(new GoogleUserDAO(accessToken, id, localDateTime));
+            googleUserRepositoryJPA.save(new GoogleUserDAO(id, accessToken, localDateTime));
             userRepositoryJPA.save(new UserDAO(createUniqueIdBean.exec(), id, name, picture));
         } else {
             googleUserDAO.setAccessToken(accessToken);
             googleUserDAO.setExpirationTime(localDateTime);
-
             googleUserRepositoryJPA.save(googleUserDAO);
+
             UserDAO userDAO = userRepositoryJPA.findByUserId(id);
             userDAO.setName(name);
             userDAO.setImage(picture);
