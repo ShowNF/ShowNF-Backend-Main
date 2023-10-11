9a46a6a7eb15a46fd2ef825517409f55caaf69ba
Lee <lsmin476@naver.com>
2023-10-11 16:41:21 +0900

    |SM UPDAET| Spring Security /login/oauth2에 대한 필터 설정

--- src/main/java/com/shownf/reptile/bean/CustomFilterBean.java
+++ src/main/java/com/shownf/reptile/bean/CustomFilterBean.java
@@ -37,30 +37,34 @@ public class CustomFilterBean extends GenericFilterBean {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String userToken = httpRequest.getHeader("access-token"); // "Your-Header-Name"을 실제 헤더 이름으로 변경하세요.
 
-
-        KakaoUserDAO kakaoUserDAO = kakaoUserRepositoryJPA.findByAccessToken(userToken);
-        GoogleUserDAO googleUserDAO = googleUserRepositoryJPA.findByAccessToken(userToken);
-
-        String savedToken;
-
-        if (kakaoUserDAO != null) savedToken = kakaoUserDAO.getAccessToken();
-        else if (googleUserDAO != null) savedToken = googleUserDAO.getAccessToken();
-        else savedToken = null;
-
-        if (userToken != null && userToken.equals(savedToken)) {
-            logger.info("사용자 정의 인가 필터: 토큰이 유효합니다. API 호출 허용");
-
-            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
-                    null, null, AuthorityUtils.createAuthorityList("ROLE_ADMIN")));
-
+        if (httpRequest.getRequestURI().substring(0, 13).equals("/login/oauth2")){
             chain.doFilter(request, response);
-        } else {
-            logger.info("사용자 정의 인가 필터: 토큰이 유효하지 않습니다. API 호출 거부");
-            // 여기서 필요에 따라 응답을 수정하거나 에러 처리를 할 수 있습니다.
-            HttpServletResponse httpResponse = (HttpServletResponse) response;
+        }
+        else {
+            KakaoUserDAO kakaoUserDAO = kakaoUserRepositoryJPA.findByAccessToken(userToken);
+            GoogleUserDAO googleUserDAO = googleUserRepositoryJPA.findByAccessToken(userToken);
 
-            // 예: 401 Unauthorized
-            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            String savedToken;
+
+            if (kakaoUserDAO != null) savedToken = kakaoUserDAO.getAccessToken();
+            else if (googleUserDAO != null) savedToken = googleUserDAO.getAccessToken();
+            else savedToken = null;
+
+            if (userToken != null && userToken.equals(savedToken)) {
+                //logger.info("사용자 정의 인가 필터: 토큰이 유효합니다. API 호출 허용");
+
+                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+                        null, null, AuthorityUtils.createAuthorityList("ROLE_ADMIN")));
+
+                chain.doFilter(request, response);
+            } else {
+                //logger.info("사용자 정의 인가 필터: 토큰이 유효하지 않습니다. API 호출 거부");
+                // 여기서 필요에 따라 응답을 수정하거나 에러 처리를 할 수 있습니다.
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+                // 예: 401 Unauthorized
+                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
         }
     }
 }
