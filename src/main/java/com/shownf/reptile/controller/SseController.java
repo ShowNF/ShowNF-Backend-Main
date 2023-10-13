6938138a0b6aeb3f9ac8cc926c906a894c127156
Lee <lsmin476@naver.com>
2023-10-13 20:24:07 +0900

    |SM UPDATE| SSE 구현 test

--- src/main/java/com/shownf/reptile/controller/SseController.java
+++ src/main/java/com/shownf/reptile/controller/SseController.java
@@ -1,2 +1,45 @@
-package com.shownf.reptile.controller;public class SseController {
+package com.shownf.reptile.controller;
+
+import com.shownf.reptile.bean.SseEmittersBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+
+@RestController
+@CrossOrigin("*")
+public class SseController {
+
+    private final SseEmittersBean sseEmittersBean;
+
+    @Autowired
+    public SseController(SseEmittersBean sseEmittersBean) {
+        this.sseEmittersBean = sseEmittersBean;
+    }
+
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect() {
+        SseEmitter emitter = new SseEmitter();
+        sseEmittersBean.add(emitter);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("connect")
+                    .data("connected!"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
+    }
+
+    @PostMapping("/count")
+    public ResponseEntity<Long> count() {
+        long count = sseEmittersBean.count();
+        return ResponseEntity.ok(count);
+    }
 }
