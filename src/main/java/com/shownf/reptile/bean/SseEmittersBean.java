5ae34bf31b40c4b8549097c8611e59d4ceb58297
Lee <lsmin476@naver.com>
2023-10-14 21:05:08 +0900

    |SM UPDATE| SSE 채팅방 아이디 별로 최최종 수정

--- src/main/java/com/shownf/reptile/bean/SseEmittersBean.java
+++ src/main/java/com/shownf/reptile/bean/SseEmittersBean.java
@@ -14,15 +14,19 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class SseEmittersBean {
 
-    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<Integer, SseEmitter> emitters = new ConcurrentHashMap<>();
 
-    public SseEmitter add(SseEmitter emitter, String chatRoomId) {
-        this.emitters.put(chatRoomId, emitter);
+    public SseEmitter add(SseEmitter emitter, String chatRoomId, String userId) {
+
+        // 채팅방 + 유저아이디 해시
+        int key = (chatRoomId + userId).hashCode();
+
+        this.emitters.put(key, emitter);
         log.info("new emitter added: {}", emitter);
         log.info("emitter list size: {}", emitters.size());
         emitter.onCompletion(() -> {
             log.info("onCompletion callback");
-            this.emitters.remove(chatRoomId);    // 만료되면 리스트에서 삭제
+            this.emitters.remove(key);    // 만료되면 리스트에서 삭제
         });
         emitter.onTimeout(() -> {
             log.info("onTimeout callback");
@@ -38,7 +42,12 @@ public class SseEmittersBean {
 
         // chatRoomId를 이용하여 해당 채팅방에만 데이터 전송
         String chatRoomId = chat.getChatId();
-        SseEmitter emitter = emitters.get(chatRoomId);
+        String userId = chat.getSenderId();
+
+        // 채팅방 + 유저아이디 해시
+        int key = (chatRoomId + userId).hashCode();
+
+        SseEmitter emitter = emitters.get(key);
         if (emitter != null) {
             try {
                 emitter.send(SseEmitter.event()
--- src/main/java/com/shownf/reptile/controller/SseController.java
+++ src/main/java/com/shownf/reptile/controller/SseController.java
@@ -22,10 +22,10 @@ public class SseController {
         this.sseEmittersBean = sseEmittersBean;
     }
 
-    @GetMapping(value = "/connect/chat-room/{chatRoomId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> connect(@PathVariable String chatRoomId) {
-        final SseEmitter emitter = new SseEmitter(60000L);
-        sseEmittersBean.add(emitter, chatRoomId);
+    @GetMapping(value = "/connect/chat-room/{chatRoomId}/user/{userId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect(@PathVariable String chatRoomId,@PathVariable String userId) {
+        final SseEmitter emitter = new SseEmitter(180000L);
+        sseEmittersBean.add(emitter, chatRoomId, userId);
         try {
             emitter.send(SseEmitter.event()
                     .name("connect")
