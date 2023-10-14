b49c1b09232b7d3d4d316c69fe87f96fb99e9044
Lee <lsmin476@naver.com>
2023-10-14 15:53:42 +0900

    |SM UPDATE| 시큐리티 CORS 추가

--- src/main/java/com/shownf/reptile/config/SecurityConfig.java
+++ src/main/java/com/shownf/reptile/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
