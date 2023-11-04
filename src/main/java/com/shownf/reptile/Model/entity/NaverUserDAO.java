f51283cb63d85cbc5e2aabc10dc4d57464ee24bb
Lee <lsmin476@naver.com>
2023-11-05 00:37:31 +0900

    |SM UPDATE| 네이버 유저 추가를 위한 DAO, Repository 추가

--- src/main/java/com/shownf/reptile/Model/entity/NaverUserDAO.java
+++ src/main/java/com/shownf/reptile/Model/entity/NaverUserDAO.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.Model.entity;public class NaverUserDAO {
+package com.shownf.reptile.Model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class NaverUserDAO {
+    @Id
+    String naverId;
+    String accessToken;
+    LocalDateTime expirationTime;
 }
--- src/main/java/com/shownf/reptile/repository/NaverUserRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/NaverUserRepositoryJPA.java
@@ -1,2 +1,9 @@
-package com.shownf.reptile.repository;public interface NaverUserRepositoryJPA {
+package com.shownf.reptile.repository;
+
+import com.shownf.reptile.Model.entity.NaverUserDAO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NaverUserRepositoryJPA extends JpaRepository<NaverUserDAO, Long> {
+    NaverUserDAO findByNaverId(String naverId);
+    NaverUserDAO findByAccessToken(String accessToken);
 }
