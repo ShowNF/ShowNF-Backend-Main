b65dba5bcf523e92d3a804519d76d1d7708b331d
Lee <lsmin476@naver.com>
2023-11-07 00:22:22 +0900

    |SM UPDATE| 다이어리 저장시 마이펫 몸무게 업데이트 기능 추가

--- src/main/java/com/shownf/reptile/bean/SaveDiaryBean.java
+++ src/main/java/com/shownf/reptile/bean/SaveDiaryBean.java
@@ -3,6 +3,7 @@ package com.shownf.reptile.bean;
 import com.shownf.reptile.Model.DTO.RequestDiarySaveDTO;
 import com.shownf.reptile.bean.small.CreateUniqueIdBean;
 import com.shownf.reptile.bean.small.SaveDiaryDAOBean;
+import com.shownf.reptile.bean.small.UpdatePetWeightDAOBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,18 +12,25 @@ public class SaveDiaryBean {
 
     CreateUniqueIdBean createUniqueIdBean;
     SaveDiaryDAOBean saveDiaryDAOBean;
+    UpdatePetWeightDAOBean updatePetWeightDAOBean;
 
     @Autowired
-    public SaveDiaryBean(CreateUniqueIdBean createUniqueIdBean, SaveDiaryDAOBean saveDiaryDAOBean) {
+    public SaveDiaryBean(CreateUniqueIdBean createUniqueIdBean, SaveDiaryDAOBean saveDiaryDAOBean, UpdatePetWeightDAOBean updatePetWeightDAOBean) {
         this.createUniqueIdBean = createUniqueIdBean;
         this.saveDiaryDAOBean = saveDiaryDAOBean;
+        this.updatePetWeightDAOBean = updatePetWeightDAOBean;
     }
 
     // 다이어리 저장
     public Long exec(RequestDiarySaveDTO requestDiarySaveDTO){
 
+        // 다이어리 수정시 저장
         if (requestDiarySaveDTO.getDiaryId() != null){
             saveDiaryDAOBean.exec(requestDiarySaveDTO);
+
+            // 다이어리 저장시 펫 몸무게 업데이트
+            updatePetWeightDAOBean.exec(requestDiarySaveDTO);
+
             return requestDiarySaveDTO.getDiaryId();
         }
 
@@ -31,6 +39,9 @@ public class SaveDiaryBean {
 
         // 다이어리 저장
         saveDiaryDAOBean.exec(diaryId, requestDiarySaveDTO);
+
+        // 다이어리 저장시 펫 몸무게 업데이트
+        updatePetWeightDAOBean.exec(requestDiarySaveDTO);
 
         // 다이어리 diaryId 반환
         return diaryId;
