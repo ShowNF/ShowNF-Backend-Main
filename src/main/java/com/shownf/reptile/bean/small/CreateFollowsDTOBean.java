7fe08d58ba36165fee6e2ac40bc2b830a11ae9ea
Lee <lsmin476@naver.com>
2023-11-08 02:40:29 +0900

    |SM UPDATE| 유저 팔로워 전체 조회 기능

--- src/main/java/com/shownf/reptile/bean/small/CreateFollowsDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateFollowsDTOBean.java
@@ -1,2 +1,28 @@
-package com.shownf.reptile.bean.small;public class CreateFollowsDTOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.ResponseFollowDTO;
+import com.shownf.reptile.Model.entity.FollowDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CreateFollowsDTOBean {
+
+    // 팔로우 DTO 변환
+    public List<ResponseFollowDTO> exec(List<FollowDAO> followDAOs){
+
+        List<ResponseFollowDTO> responseFollowDTOs = new ArrayList<>();
+
+        for (FollowDAO followDAO : followDAOs){
+            ResponseFollowDTO responseFollowDTO = new ResponseFollowDTO();
+
+            responseFollowDTO.setFollowId(followDAO.getFollowId());
+            responseFollowDTO.setFollowUserId(followDAO.getFollowUserId());
+            responseFollowDTOs.add(responseFollowDTO);
+        }
+
+        return responseFollowDTOs;
+    }
 }
--- src/main/java/com/shownf/reptile/bean/small/GetFollowsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetFollowsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetFollowsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.FollowDAO;
+import com.shownf.reptile.repository.FollowRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetFollowsDAOBean {
+
+    FollowRepositoryJPA followRepositoryJPA;
+
+    @Autowired
+    public GetFollowsDAOBean(FollowRepositoryJPA followRepositoryJPA) {
+        this.followRepositoryJPA = followRepositoryJPA;
+    }
+
+    // 유저아이디로 팔로우 전체 찾기
+    public List<FollowDAO> exec(Long userId){
+        return followRepositoryJPA.findByUserId(userId);
+    }
 }
