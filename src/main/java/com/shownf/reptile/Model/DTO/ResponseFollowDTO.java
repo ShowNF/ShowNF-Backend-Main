68744b389363788ba670122311046ee4361fdd4b
Lee <lsmin476@naver.com>
2023-11-08 02:40:08 +0900

    |SM UPDATE| DTO, Repository responseDTO 생성 및 findByuserId jpa 추가

--- src/main/java/com/shownf/reptile/Model/DTO/ResponseFollowDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/ResponseFollowDTO.java
@@ -1,2 +1,9 @@
-package com.shownf.reptile.Model.DTO;public class ResponseFollowDTO {
+package com.shownf.reptile.Model.DTO;
+
+import lombok.Data;
+
+@Data
+public class ResponseFollowDTO {
+    Long followId;
+    Long followUserId;
 }
--- src/main/java/com/shownf/reptile/repository/FollowRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/FollowRepositoryJPA.java
@@ -3,6 +3,10 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.Model.entity.FollowDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FollowRepositoryJPA extends JpaRepository<FollowDAO, Long> {
     FollowDAO findByUserIdAndFollowUserId(Long userId, Long followUserId);
+
+    List<FollowDAO> findByUserId(Long userId);
 }
