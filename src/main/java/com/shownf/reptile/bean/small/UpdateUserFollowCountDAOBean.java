c4ad8424abc2f757a761ab05b47f923ee0ee7621
Lee <lsmin476@naver.com>
2023-11-08 02:10:40 +0900

    |SM UPDATE| 유저 팔로우 취소에 따른 팔로워 수 팔로잉 수 감소

--- src/main/java/com/shownf/reptile/bean/small/UpdateUserFollowCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateUserFollowCountDAOBean.java
@@ -35,7 +35,30 @@ public class UpdateUserFollowCountDAOBean {
 
         // 팔로우 팔로잉 1씩 증가
         userDAO.setFollowerCount(userDAO.getFollowerCount() + 1);
-        followedUserDAO.setFollowingCount(userDAO.getFollowingCount() + 1);
+        followedUserDAO.setFollowingCount(followedUserDAO.getFollowingCount() + 1);
+
+        saveUserDAOBean.exec(userDAO);
+        saveUserDAOBean.exec(followedUserDAO);
+    }
+
+    // 팔로우 취소시 유저 팔로우수와 팔로우 당한 유저 팔로잉수 감소
+    public void exec(Long followId, FollowDAO followDAO){
+
+        // userId 가져오기
+        Long userId = followDAO.getUserId();
+
+        // 팔로우한 유저 객체 가져오기
+        UserDAO userDAO = userRepositoryJPA.findById(userId).get();
+
+        // followUserId 가져오기
+        Long followUserId = followDAO.getFollowUserId();
+
+        // 팔로우 당한 유저 객체 가져오기
+        UserDAO followedUserDAO = userRepositoryJPA.findById(followUserId).get();
+
+        // 팔로우 팔로잉 1씩 감소
+        userDAO.setFollowerCount(userDAO.getFollowerCount() - 1);
+        followedUserDAO.setFollowingCount(followedUserDAO.getFollowingCount() - 1);
 
         saveUserDAOBean.exec(userDAO);
         saveUserDAOBean.exec(followedUserDAO);
