5f0cdad8f6f0dcaf65978367afb66a063f3647ab
Lee <lsmin476@naver.com>
2023-11-12 22:48:30 +0900

    |SM UPDATE| 댓글 저장시 게시물 댓글 개수 감소 로직 수정

--- src/main/java/com/shownf/reptile/bean/small/UpdatePostCommentCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdatePostCommentCountDAOBean.java
@@ -2,6 +2,7 @@ package com.shownf.reptile.bean.small;
 
 import com.shownf.reptile.Model.entity.CommentDAO;
 import com.shownf.reptile.Model.entity.PostDAO;
+import com.shownf.reptile.repository.CommentRepositoryJPA;
 import com.shownf.reptile.repository.PostRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Component;
 public class UpdatePostCommentCountDAOBean {
 
     PostRepositoryJPA postRepositoryJPA;
+    CommentRepositoryJPA commentRepositoryJPA;
 
     @Autowired
-    public UpdatePostCommentCountDAOBean(PostRepositoryJPA postRepositoryJPA) {
+    public UpdatePostCommentCountDAOBean(PostRepositoryJPA postRepositoryJPA, CommentRepositoryJPA commentRepositoryJPA) {
         this.postRepositoryJPA = postRepositoryJPA;
+        this.commentRepositoryJPA = commentRepositoryJPA;
     }
 
     // 게시물 댓글 갯수 추가
@@ -41,8 +44,12 @@ public class UpdatePostCommentCountDAOBean {
         // postId 로 게시물 찾기
         PostDAO postDAO = postRepositoryJPA.findById(postId).get();
 
-        // 게시물 댓글 수 1 감소
-        postDAO.setCommentCount(postDAO.getCommentCount() - 1);
+        // 게시물 댓글 수 감소
+        Integer replyCount = commentRepositoryJPA.findById(commentId).get().getReplyCount();
+        if (replyCount == 0)
+            postDAO.setCommentCount(postDAO.getCommentCount() - 1);
+        else
+            postDAO.setCommentCount(postDAO.getCommentCount() - 1 - replyCount);
 
         // 게시물 반환
         return postDAO;
