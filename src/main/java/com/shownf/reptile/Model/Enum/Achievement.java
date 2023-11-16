1896e0b58626af5742638cf753e5c7bb5fa8b779
Lee <lsmin476@naver.com>
2023-11-16 23:46:05 +0900

    |SM UPDATE| 업적 DAO 추가

---
+++ src/main/java/com/shownf/reptile/Model/Enum/Achievement.java
@@ -0,0 +1,5 @@
+package com.shownf.reptile.Model.Enum;
+
+public enum Achievement {
+    공감왕, 이구역인싸, 멋진작가, 멋진코멘더, 브리더, 성실왕, 소통왕, 분양왕, 입양왕
+}
---
+++ src/main/java/com/shownf/reptile/Model/entity/AchievementsDAO.java
@@ -0,0 +1,28 @@
+package com.shownf.reptile.Model.entity;
+
+import com.shownf.reptile.Model.Enum.Achievement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class AchievementsDAO {
+    @Id
+    Achievement achievement;
+    String achievementName;
+    String icon;
+    String achievementExplain;
+    Integer bronze;
+    Integer silver;
+    Integer gold;
+    Integer platinum;
+    Integer diamond;
+}
