6d277108622e41eb357a31449a4e8a02eb6e265e
Lee <lsmin476@naver.com>
2023-12-05 01:43:14 +0900

    |SM UPDATE| Repository findByUserId() 추가

--- src/main/java/com/shownf/reptile/repository/ReplyHeartRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/ReplyHeartRepositoryJPA.java
@@ -4,7 +4,11 @@ import com.shownf.reptile.Model.entity.CommentHeartDAO;
 import com.shownf.reptile.Model.entity.ReplyHeartDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyHeartRepositoryJPA extends JpaRepository<ReplyHeartDAO, Long> {
+
+    List<ReplyHeartDAO> findByUserId(Long userId);
 
     ReplyHeartDAO findByUserIdAndReplyId(Long userId, Long replyId);
 }
