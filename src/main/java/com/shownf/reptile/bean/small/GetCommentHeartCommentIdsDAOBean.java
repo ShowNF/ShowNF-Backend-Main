9e0af223f6bcbb178943842cc2f6f4c9bb5b1b66
Lee <lsmin476@naver.com>
2023-12-05 01:32:51 +0900

    |SM UPDATE| 유저 아이디로 유저 전체 조회 및 댓글 아이디 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetCommentHeartCommentIdsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetCommentHeartCommentIdsDAOBean.java
@@ -1,2 +1,21 @@
-package com.shownf.reptile.bean.small;public class GetCommentHeartCommentIdsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.CommentHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetCommentHeartCommentIdsDAOBean {
+
+    // 좋아요 누른 댓글 아이디 가져오기
+    public List<Long> exec(List<CommentHeartDAO> commentHeartDAOS){
+        List<Long> commentIds = new ArrayList<>();
+
+        for (CommentHeartDAO commentHeartDAO : commentHeartDAOS)
+            commentIds.add(commentHeartDAO.getCommentId());
+
+        return commentIds;
+    }
 }
--- src/main/java/com/shownf/reptile/bean/small/GetCommentHeartsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetCommentHeartsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetCommentHeartsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.CommentHeartDAO;
+import com.shownf.reptile.repository.CommentHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetCommentHeartsDAOBean {
+
+    CommentHeartRepositoryJPA commentHeartRepositoryJPA;
+
+    @Autowired
+    public GetCommentHeartsDAOBean(CommentHeartRepositoryJPA commentHeartRepositoryJPA) {
+        this.commentHeartRepositoryJPA = commentHeartRepositoryJPA;
+    }
+
+    // 유저가 누른 좋아요 찾기
+    public List<CommentHeartDAO> exec(Long userId){
+        return commentHeartRepositoryJPA.findByUserId(userId);
+    }
 }
