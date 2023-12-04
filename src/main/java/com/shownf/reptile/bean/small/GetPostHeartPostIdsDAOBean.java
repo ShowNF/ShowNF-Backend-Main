fc6311d75d39f8db814eb9018dc941cc2f56c7a4
Lee <lsmin476@naver.com>
2023-12-05 01:21:23 +0900

    |SM UPDATE| 좋아요 누른 전체 게시물 아이디 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetPostHeartPostIdsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostHeartPostIdsDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetPostHeartPostIdsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.PostHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetPostHeartPostIdsDAOBean {
+
+    // 좋아요 눌린 게시물 아이디 가져오기
+    public List<Long> exec(List<PostHeartDAO> postHeartDAOS){
+
+        List<Long> postIds = new ArrayList<>();
+
+        for (PostHeartDAO postHeartDAO : postHeartDAOS)
+            postIds.add(postHeartDAO.getPostId());
+
+        return postIds;
+    }
 }
