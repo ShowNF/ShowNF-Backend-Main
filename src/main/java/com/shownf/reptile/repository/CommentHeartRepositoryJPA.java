963fc55ba1dd15d4ac829d675393e5cf635445f3
Lee <lsmin476@naver.com>
2023-12-05 01:32:20 +0900

    |SM UPDATE| Repository findByUserId() 추가

--- src/main/java/com/shownf/reptile/repository/CommentHeartRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/CommentHeartRepositoryJPA.java
@@ -3,7 +3,11 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.Model.entity.CommentHeartDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentHeartRepositoryJPA extends JpaRepository<CommentHeartDAO, Long> {
+
+    List<CommentHeartDAO> findByUserId(Long userId);
 
     CommentHeartDAO findByUserIdAndCommentId(Long userId, Long commentId);
 }
