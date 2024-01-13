ff4e189f9471d2b6e73943c5ccd2b3271de95025
Lee <lsmin476@naver.com>
2024-01-14 03:36:00 +0900

    |SM UPDATE| token validation check 시큐리티 해제안된 문제 해결

--- src/main/java/com/shownf/reptile/config/SecurityConfig.java
+++ src/main/java/com/shownf/reptile/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/login/oauth2/**", "/", "/test/**","/chat-event", "/connect/**").permitAll() // 로그인, health 체크
-                .antMatchers("/user/**/name", "/user/**/image", "user/**/token").permitAll()// 유저 이름, 이미지 정보
+                .antMatchers("/user/**/name", "/user/**/image", "/user/**/token").permitAll()// 유저 이름, 이미지 정보
                 .antMatchers("/swagger-ui.html","swagger/**","/webjars/**", "/swagger-resources/**", "/v2/api-docs").permitAll() // 스웨거
                 .antMatchers(HttpMethod.GET, "/image", "/comment/**", "/post/**", "/reply/**", "/sale/**").permitAll() // 스와이프
                 .antMatchers("/**").hasRole("USER")
