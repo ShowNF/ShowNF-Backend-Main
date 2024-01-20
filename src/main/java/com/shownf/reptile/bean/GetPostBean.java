bb6cdca7b37bc779f74f6f332fd75186226890e6
Lee <lsmin476@naver.com>
2024-01-20 14:44:46 +0900

    |SM UPDATE| 게시물 조회시 게시물 메타데이터 갱신

--- src/main/java/com/shownf/reptile/bean/GetPostBean.java
+++ src/main/java/com/shownf/reptile/bean/GetPostBean.java
@@ -1,6 +1,7 @@
 package com.shownf.reptile.bean;
 
 import com.shownf.reptile.Model.DTO.RequestPostDTO;
+import com.shownf.reptile.Model.MetaDAO.PostMeta;
 import com.shownf.reptile.bean.small.*;
 import com.shownf.reptile.Model.entity.PostDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +10,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class GetPostBean {
     GetPostDAOBean getPostDAOBean;
+    GetPostMetaDAOBean getPostMetaDAOBean;
     UpdatePostViewCountDAOBean updatePostViewCountDAOBean;
     CreatePostDTOBean createPostDTOBean;
     SavePostDAOBean savePostDAOBean;
+    SavePostMetaDAOBean savePostMetaDAOBean;
     SavePostLogDAOBean savePostLogDAOBean;
 
     @Autowired
-    public GetPostBean(GetPostDAOBean getPostDAOBean, UpdatePostViewCountDAOBean updatePostViewCountDAOBean, CreatePostDTOBean createPostDTOBean, SavePostDAOBean savePostDAOBean, SavePostLogDAOBean savePostLogDAOBean) {
+    public GetPostBean(GetPostDAOBean getPostDAOBean, GetPostMetaDAOBean getPostMetaDAOBean, UpdatePostViewCountDAOBean updatePostViewCountDAOBean, CreatePostDTOBean createPostDTOBean, SavePostDAOBean savePostDAOBean, SavePostMetaDAOBean savePostMetaDAOBean, SavePostLogDAOBean savePostLogDAOBean) {
         this.getPostDAOBean = getPostDAOBean;
+        this.getPostMetaDAOBean = getPostMetaDAOBean;
         this.updatePostViewCountDAOBean = updatePostViewCountDAOBean;
         this.createPostDTOBean = createPostDTOBean;
         this.savePostDAOBean = savePostDAOBean;
+        this.savePostMetaDAOBean = savePostMetaDAOBean;
         this.savePostLogDAOBean = savePostLogDAOBean;
     }
 
@@ -30,14 +35,24 @@ public class GetPostBean {
         if (postDAO == null) return null;
 
         // 게시물 찾기로 인한 조회수 1 증가
-        PostDAO findPostDAO = updatePostViewCountDAOBean.exec(postDAO);
-        if (findPostDAO == null) return null;
+        PostDAO updatePostDAO = updatePostViewCountDAOBean.exec(postDAO);
+        if (updatePostDAO == null) return null;
 
         // DTO 에 게시물 객체 넘기기
-        RequestPostDTO requestPostDTO = createPostDTOBean.exec(findPostDAO);
+        RequestPostDTO requestPostDTO = createPostDTOBean.exec(updatePostDAO);
+
+        // postMeta 게시물 찾기
+        PostMeta postMeta = getPostMetaDAOBean.exec(postId);
+        if (postMeta == null) return null;
+
+        // 게시물 메타데이터 조회수 증가
+        PostMeta updatePostMeta = updatePostViewCountDAOBean.exec(postMeta);
 
         // 게시물 저장
-        savePostDAOBean.exec(findPostDAO);
+        savePostDAOBean.exec(updatePostDAO);
+
+        // 게시물 메타데이터 저장
+        savePostMetaDAOBean.exec(updatePostMeta);
 
         // 최근 조회 게시물 추가
         savePostLogDAOBean.exec(postId, userId);
--- src/main/java/com/shownf/reptile/bean/small/UpdatePostViewCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdatePostViewCountDAOBean.java
@@ -1,6 +1,8 @@
 package com.shownf.reptile.bean.small;
 
+import com.shownf.reptile.Model.MetaDAO.PostMeta;
 import com.shownf.reptile.Model.entity.PostDAO;
+import com.shownf.reptile.repository.PostMetaRepositoryJPA;
 import com.shownf.reptile.repository.PostRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,10 +11,12 @@ import org.springframework.stereotype.Component;
 public class UpdatePostViewCountDAOBean {
 
     PostRepositoryJPA postRepositoryJPA;
+    PostMetaRepositoryJPA postMetaRepositoryJPA;
 
     @Autowired
-    public UpdatePostViewCountDAOBean(PostRepositoryJPA postRepositoryJPA) {
+    public UpdatePostViewCountDAOBean(PostRepositoryJPA postRepositoryJPA, PostMetaRepositoryJPA postMetaRepositoryJPA) {
         this.postRepositoryJPA = postRepositoryJPA;
+        this.postMetaRepositoryJPA = postMetaRepositoryJPA;
     }
 
     // 게시물 찾기에 대한 조회수 1 증가
@@ -30,5 +34,22 @@ public class UpdatePostViewCountDAOBean {
 
         // 게시물 반환
         return postDAO1;
+    }
+
+    // 메타데이터 조회수 1 증가
+    public PostMeta exec(PostMeta postMeta){
+
+        // postId 가져오기
+        Long postId = postMeta.getPostId();
+
+        // postId 로 게시물 찾기
+        PostMeta updatePostMeta = postMetaRepositoryJPA.findById(postId).orElse(null);
+        if (updatePostMeta == null) return null;
+
+        // 게시물 조회수 1 증가
+        updatePostMeta.setViewCount(postMeta.getViewCount() + 1);
+
+        // 게시물 반환
+        return postMeta;
     }
 }
