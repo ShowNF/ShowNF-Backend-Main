f55985eb6d2c536f3c8ce4b2a14524635f849db2
Lee <lsmin476@naver.com>
2024-01-22 00:21:35 +0900

    |SM UPDATE| 메타 데이터 전체 조회시 content id 반환에서 postContent 내용 반환으로 변경

--- src/main/java/com/shownf/reptile/bean/small/CreatePostMetaDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreatePostMetaDTOBean.java
@@ -1,14 +1,27 @@
 package com.shownf.reptile.bean.small;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shownf.reptile.Model.DTO.ResponsePostMetaDTO;
 import com.shownf.reptile.Model.MetaDAO.PostMeta;
+import com.shownf.reptile.Model.entity.PostContentDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class CreatePostMetaDTOBean {
+
+    GetPostContentDAOBean getPostContentDAOBean;
+
+    @Autowired
+    public CreatePostMetaDTOBean(GetPostContentDAOBean getPostContentDAOBean) {
+        this.getPostContentDAOBean = getPostContentDAOBean;
+    }
 
     // Create the post meta DTO
     public ResponsePostMetaDTO exec(PostMeta postMeta){
@@ -19,7 +32,33 @@ public class CreatePostMetaDTOBean {
         responsePostMetaDTO.setPostId(postMeta.getPostId());
         responsePostMetaDTO.setUserId(postMeta.getUserId());
         responsePostMetaDTO.setTitle(postMeta.getTitle());
-        responsePostMetaDTO.setContent(postMeta.getContent());
+
+        // 게시물 내용
+        PostContentDAO postContentDAO = getPostContentDAOBean.exec(Long.parseLong(postMeta.getContent()));
+
+        System.out.println("postContentDAO = " + postContentDAO.getContent());
+        System.out.println("postContentDAO.getImageUrl() = " + postContentDAO.getImageUrl());
+        
+        List<Map<String, String>> resultList = new ArrayList<>();
+        Map<String, String> postContentMap = new HashMap<>();
+
+        // imageUrl과 content를 Map에 추가
+        postContentMap.put("imageUrl", postContentDAO.getImageUrl());
+        postContentMap.put("content", postContentDAO.getContent());
+
+        // 리스트에 추가
+        resultList.add(postContentMap);
+
+        ObjectMapper objectMapper =new ObjectMapper();
+
+        String content = "";
+        try {
+            content = objectMapper.writeValueAsString(resultList);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        responsePostMetaDTO.setContent(content);
         responsePostMetaDTO.setCategory(postMeta.getCategory().name());
         responsePostMetaDTO.setUploadTime(postMeta.getUploadTime());
         responsePostMetaDTO.setUpdateTime(postMeta.getUpdateTime());
