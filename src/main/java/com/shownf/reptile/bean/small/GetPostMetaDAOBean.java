061d7ab22df7fd6eff084187ef2430805c20110e
Lee <lsmin476@naver.com>
2024-01-21 23:38:29 +0900

    |SM UPDATE| Post Meta deleteCheck 필드 추가

--- src/main/java/com/shownf/reptile/bean/small/GetPostMetaDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostMetaDAOBean.java
@@ -1,8 +1,11 @@
 package com.shownf.reptile.bean.small;
 
 import com.shownf.reptile.Model.MetaDAO.PostMeta;
+import com.shownf.reptile.Model.entity.PostDAO;
 import com.shownf.reptile.repository.PostMetaRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -25,5 +28,10 @@ public class GetPostMetaDAOBean {
     // Get post metas
     public List<PostMeta> exec(List<Long> postIds){
         return postMetaRepositoryJPA.findAllById(postIds);
+    }
+
+    // Get post metas from userId
+    public Page<PostMeta> exec(Long userId, Pageable pageable){
+        return postMetaRepositoryJPA.findByUserId(userId, pageable);
     }
 }
