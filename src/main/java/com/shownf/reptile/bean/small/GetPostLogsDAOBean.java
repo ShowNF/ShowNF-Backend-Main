656c174122fe4b6aba61a376f2cbe07b7f054d69
Lee <lsmin476@naver.com>
2024-01-22 02:40:46 +0900

    |SM UPDATE| 최근 게시물 조회시 제일 최근 조회한 순으로 정렬 추가

--- src/main/java/com/shownf/reptile/bean/small/GetPostLogsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostLogsDAOBean.java
@@ -3,6 +3,7 @@ package com.shownf.reptile.bean.small;
 import com.shownf.reptile.Model.entity.PostLogDAO;
 import com.shownf.reptile.repository.PostLogRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class GetPostLogsDAOBean {
 
     // Get post logs
     public List<PostLogDAO> exec(Long userId){
-        return postLogRepositoryJPA.findByUserId(userId);
+        Sort sort = Sort.by(Sort.Order.desc("viewTime"));
+        return postLogRepositoryJPA.findByUserId(userId, sort);
     }
 }
--- src/main/java/com/shownf/reptile/repository/PostLogRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/PostLogRepositoryJPA.java
@@ -1,6 +1,7 @@
 package com.shownf.reptile.repository;
 
 import com.shownf.reptile.Model.entity.PostLogDAO;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 @Repository
 public interface PostLogRepositoryJPA extends JpaRepository<PostLogDAO, Long> {
 
-    List<PostLogDAO> findByUserId(Long userId);
+    List<PostLogDAO> findByUserId(Long userId, Sort sort);
 }
