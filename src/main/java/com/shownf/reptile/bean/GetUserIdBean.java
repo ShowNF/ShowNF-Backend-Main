9cda61e38fdec8bf2fab28ce8f4ae6c09f359d9e
Lee <lsmin476@naver.com>
2024-02-03 18:38:04 +0900

    |SM UPDATE| 네이버 Id 가져오기 기능 수정

--- src/main/java/com/shownf/reptile/bean/GetUserIdBean.java
+++ src/main/java/com/shownf/reptile/bean/GetUserIdBean.java
@@ -2,8 +2,10 @@ package com.shownf.reptile.bean;
 
 import com.shownf.reptile.Model.entity.GoogleUserDAO;
 import com.shownf.reptile.Model.entity.KakaoUserDAO;
+import com.shownf.reptile.Model.entity.NaverUserDAO;
 import com.shownf.reptile.repository.GoogleUserRepositoryJPA;
 import com.shownf.reptile.repository.KakaoUserRepositoryJPA;
+import com.shownf.reptile.repository.NaverUserRepositoryJPA;
 import com.shownf.reptile.repository.UserRepositoryJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,24 +15,29 @@ public class GetUserIdBean {
 
     KakaoUserRepositoryJPA kakaoUserRepositoryJPA;
     GoogleUserRepositoryJPA googleUserRepositoryJPA;
+    NaverUserRepositoryJPA naverUserRepositoryJPA;
     UserRepositoryJPA userRepositoryJPA;
 
     @Autowired
-    public GetUserIdBean(KakaoUserRepositoryJPA kakaoUserRepositoryJPA, GoogleUserRepositoryJPA googleUserRepositoryJPA, UserRepositoryJPA userRepositoryJPA) {
+    public GetUserIdBean(KakaoUserRepositoryJPA kakaoUserRepositoryJPA, GoogleUserRepositoryJPA googleUserRepositoryJPA, NaverUserRepositoryJPA naverUserRepositoryJPA, UserRepositoryJPA userRepositoryJPA) {
         this.kakaoUserRepositoryJPA = kakaoUserRepositoryJPA;
         this.googleUserRepositoryJPA = googleUserRepositoryJPA;
+        this.naverUserRepositoryJPA = naverUserRepositoryJPA;
         this.userRepositoryJPA = userRepositoryJPA;
     }
 
     public Long exec(String token){
         KakaoUserDAO kakaoUserDAO = kakaoUserRepositoryJPA.findByAccessToken(token);
         GoogleUserDAO googleUserDAO = googleUserRepositoryJPA.findByAccessToken(token);
+        NaverUserDAO naverUserDAO = naverUserRepositoryJPA.findByAccessToken(token);
         String oauthId;
 
         if (kakaoUserDAO != null)
             oauthId = kakaoUserDAO.getKakaoId();
         else if (googleUserDAO != null)
             oauthId = googleUserDAO.getGoogleId();
+        else if (naverUserDAO != null)
+            oauthId = naverUserDAO.getNaverId();
         else oauthId = null;
 
         Long userId = userRepositoryJPA.findByOauthId(oauthId).getUserId();
