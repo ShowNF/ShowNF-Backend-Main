ff93865513ae20c7c714465f406eaf01c3d977ee
Lee <lsmin476@naver.com>
2024-02-12 17:50:15 +0900

    |SM UPDATE| 좋아요 누른 분양글 조회 bean 추가

--- src/main/java/com/shownf/reptile/bean/small/GetSaleHeartSaleIdsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetSaleHeartSaleIdsDAOBean.java
@@ -1,2 +1,21 @@
-package com.shownf.reptile.bean.small;public class GetSaleHeartSaleIdsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetSaleHeartSaleIdsDAOBean {
+
+    // 좋아요 누른 분양글 아이디 가져오기
+    public List<Long> exec(List<SaleHeartDAO> saleHeartDAOS){
+        List<Long> saleHeartIds = new ArrayList<>();
+
+        for (SaleHeartDAO saleHeartDAO : saleHeartDAOS)
+            saleHeartIds.add(saleHeartDAO.getSaleId());
+
+        return saleHeartIds;
+    }
 }
