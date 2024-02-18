338e1be7bf1c878388c8dee3c6be80afc584f92d
Lee <lsmin476@naver.com>
2024-02-18 21:50:01 +0900

    |SM TEST| CICD test7

--- src/test/java/com/shownf/reptile/ReptileApplicationTests.java
+++ src/test/java/com/shownf/reptile/ReptileApplicationTests.java
@@ -2,8 +2,12 @@ package com.shownf.reptile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"AWS_S3_ACCESSKEY=" // AWS 관련 프로퍼티를 빈 값으로 설정하여 오버라이드
+})
 class ReptileApplicationTests {
 
 	@Test
