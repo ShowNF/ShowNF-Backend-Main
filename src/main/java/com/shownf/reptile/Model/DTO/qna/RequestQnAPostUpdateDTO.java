78db53e061f7437fd33d80ad4327a89030e25303
Lee <lsmin476@naver.com>
2024-02-26 16:17:59 +0900

    |SM UPDATE|  QnA 게시물 수정 로직 및 DTO 정의

--- src/main/java/com/shownf/reptile/Model/DTO/qna/RequestQnAPostUpdateDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/qna/RequestQnAPostUpdateDTO.java
@@ -1,2 +1,12 @@
-package com.shownf.reptile.Model.DTO.qna;public class RequestQnAPostUpdateDTO {
+package com.shownf.reptile.Model.DTO.qna;
+
+import lombok.Data;
+
+@Data
+public class RequestQnAPostUpdateDTO {
+    Long qnaPostId;
+    Long userId;
+    String title;
+    String content;
+    String imageUrl;
 }
--- src/main/java/com/shownf/reptile/bean/small/UpdateQnAPostDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateQnAPostDAOBean.java
@@ -1,11 +1,19 @@
 package com.shownf.reptile.bean.small;
 
-import com.shownf.reptile.Model.entity.PostDAO;
+import com.shownf.reptile.Model.DTO.qna.RequestQnAPostUpdateDTO;
 import com.shownf.reptile.Model.entity.qna.QnAPostDAO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UpdateQnAPostDAOBean {
+
+    // QnA 게시물 수정
+    public void exec(RequestQnAPostUpdateDTO requestQnAPostUpdateDTO, QnAPostDAO qnAPostDAO){
+
+        qnAPostDAO.setTitle(requestQnAPostUpdateDTO.getTitle());
+        qnAPostDAO.setContent(requestQnAPostUpdateDTO.getContent());
+        qnAPostDAO.setImageUrl(requestQnAPostUpdateDTO.getImageUrl());
+    }
 
     // QnA 게시물 찾기에 대한 조회수 1 증가
     public void exec(QnAPostDAO qnAPostDAO){
